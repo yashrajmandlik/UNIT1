bb93cfe47bdbd7ca519bd7fe3703cbacb691c738
yashrajmandlik <mandlikyashraj143@gmail.com>
2024-10-27 16:16:02 +0530

    Update ternaryoperator.c

--- Unit 1/ternaryoperator.c
+++ Unit 1/ternaryoperator.c
@@ -8,8 +8,9 @@ int main()   {
     a =10;
     b=50;
 
-            max = ("%d\n",max);             //using ternary oerator as i put condition if (a>b) print a, else print b
-
+            max = ("%d\n",max=(a > b) ? a : b;             //using ternary oerator as i put condition if (a>b) print a, else print b
+            
+    printf("%d",max);
 
     return 0;
 }
